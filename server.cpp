906775e4829fc3016543ca597bc4336eca8ad105
Yullitsael <Ricardodpinedap@gmail.com>
2026-03-17 15:31:12 -0400

    start

--- server.cpp
+++ server.cpp
@@ -1,17 +1,30 @@
 #include "crow_all.h"
 #include <fstream>
 
+9
 int main()
 {
     crow::SimpleApp app;
-
-    // Root route
-    CROW_ROUTE(app, "/")([](){
-        std::ifstream file("html/index.html");
-        std::string content((std::istreambuf_iterator<char>(file)),
-                             std::istreambuf_iterator<char>());
-        return content;
+    CROW_ROUTE(app, "/")
+    ([](const request &req, response &res) {
+        ifstream in("index.html",
+            ifstream::in);
+        if (in) {
+            ostringstream contents;
+            contents << in.rdbuf();
+            in.close();
+            res.write(contents.str());
+        }
+        else {
+            res.write("Not Found");
+        }
+        res.end();
     });
 
+    CROW_ROUTE (app, "/product/<int>")
+    ([](const request &req, response &res, int
+
     app.port(27000).multithreaded().run();
+
+    return 1;
 }
